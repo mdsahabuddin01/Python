b661a5e60c16d8ab2bd802bdac9b7a4f967ecd23
MD SAHABUDDIN <hazbun.bpl@gmail.com>
2025-03-15 09:22:45 +0000

    1_1 fc

---
+++ Python_for_finance/average_revenue.py
@@ -0,0 +1,17 @@
+
+
+revenue_1 = 229.23
+
+
+revenue_2 = 177.86
+
+
+revenue_3 = 89.95
+
+# Print the total revenue
+total = revenue_1 + revenue_2 + revenue_3
+print(total)
+
+# Print the average revenue
+average = total/3
+print(average)
