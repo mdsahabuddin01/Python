00bc9f0358d50de090e7074116c7af40e00cde50
MD SAHABUDDIN <hazbun.bpl@gmail.com>
2025-03-20 12:01:49 +0000

    Generating_a_sequence_of_numbers.py

---
+++ Python_for_finance/Generating_a_sequence_of_numbers.py
@@ -0,0 +1,9 @@
+# Create and print company IDs
+#Create an array company_ids containing the numbers 1 through 7 (inclusive).
+company_ids = np.arange(1, 8, 1)
+print(company_ids)
+
+# Use array slicing to select specific company IDs
+#Create an array company_ids_odd containing only the odd numbers from 1 through 7 (inclusive).
+company_ids_odd = np.arange(1, 8, 2)
+print(company_ids_odd)
